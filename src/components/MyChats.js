8f8b41c0330121a8f2920ccee0fcc7e780cf6f8c
Thang Vo <thang03032000@gmail.com>
2022-09-11 00:14:05 +0700

    fix bug

--- src/components/MyChats.js
+++ src/components/MyChats.js
@@ -1,62 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { useAppContext } from "../context/ChatProvider";
-import { AddIcon } from "@chakra-ui/icons";
-import { Box, Stack, Text } from "@chakra-ui/react";
-import api from "../utils/axios";
-import { Button } from "@chakra-ui/react";
-import ChatLoading from "./ChatLoading";
-import GroupChatModal from "./GroupChatModal";
-import { toast } from "react-toastify";
-import { getUserFromLocalStorage } from "../utils/localStorage";
-import { getSender } from "../config/chat";
+import React, { useState, useEffect } from 'react';
+import { useAppContext } from '../context/ChatProvider';
+import { AddIcon } from '@chakra-ui/icons';
+import { Box, Stack, Text } from '@chakra-ui/react';
+import api from '../utils/axios';
+import { Button } from '@chakra-ui/react';
+import ChatLoading from './ChatLoading';
+import GroupChatModal from './GroupChatModal';
+import { toast } from 'react-toastify';
+import { getUserFromLocalStorage } from '../utils/localStorage';
+import { getSender } from '../config/chat';
 
 const MyChats = ({ fetchAgain }) => {
   const [loggedUser, setLoggedUser] = useState();
 
   const { selectedChat, setSelectedChat, chats, setChats } = useAppContext();
 
-  const fetchChats = async () => {
-    try {
-      const { data } = await api.get("/api/v1/chat");
-
-      setChats(data);
-    } catch (error) {
-      toast.error(error);
-    }
-  };
-
   useEffect(() => {
-    setLoggedUser(getUserFromLocalStorage("user"));
+    const fetchChats = async () => {
+      try {
+        const { data } = await api.get('/api/v1/chat');
+
+        setChats(data);
+      } catch (error) {
+        toast.error(error);
+      }
+    };
+    setLoggedUser(getUserFromLocalStorage('user'));
     fetchChats();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchAgain]);
 
   return (
     <Box
-      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
-      flexDirection="column"
-      alignItems="center"
+      display={{ base: selectedChat ? 'none' : 'flex', md: 'flex' }}
+      flexDirection='column'
+      alignItems='center'
       p={3}
-      bg="white"
-      w={{ base: "100%", md: "31%" }}
-      borderRadius="lg"
-      borderWidth="1px"
+      bg='white'
+      w={{ base: '100%', md: '31%' }}
+      borderRadius='lg'
+      borderWidth='1px'
     >
       <Box
         pb={3}
         px={3}
-        fontSize={{ base: "28px", md: "30px" }}
-        fontFamily="Poppins"
-        display="flex"
-        w="100%"
-        justifyContent="space-between"
-        alignItems="center"
+        fontSize={{ base: '28px', md: '30px' }}
+        fontFamily='Poppins'
+        display='flex'
+        w='100%'
+        justifyContent='space-between'
+        alignItems='center'
       >
         My Chats
         <GroupChatModal>
           <Button
-            display="flex"
-            fontFamily="Poppins"
-            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            display='flex'
+            fontFamily='Poppins'
+            fontSize={{ base: '17px', md: '10px', lg: '17px' }}
             rightIcon={<AddIcon />}
           >
             New Group Chat
@@ -64,28 +64,28 @@ const MyChats = ({ fetchAgain }) => {
         </GroupChatModal>
       </Box>
       <Box
-        display="flex"
-        flexDirection="column"
+        display='flex'
+        flexDirection='column'
         p={3}
-        bg="#F8F8F8"
-        w="100%"
-        h="100%"
-        borderRadius="lg"
-        overflowY="hidden"
+        bg='#F8F8F8'
+        w='100%'
+        h='100%'
+        borderRadius='lg'
+        overflowY='hidden'
       >
         {chats ? (
-          <Stack overflowY="scroll">
+          <Stack overflowY='scroll'>
             {chats?.map((chat) => (
               <Box
                 onClick={() => setSelectedChat(chat)}
-                cursor="pointer"
+                cursor='pointer'
                 bg={
-                  selectedChat === chat ? "rgba(67, 43, 255, 0.8)" : "#E8E8E8"
+                  selectedChat === chat ? 'rgba(67, 43, 255, 0.8)' : '#E8E8E8'
                 }
-                color={selectedChat === chat ? "white" : "black"}
+                color={selectedChat === chat ? 'white' : 'black'}
                 px={3}
                 py={2}
-                borderRadius="lg"
+                borderRadius='lg'
                 key={chat?._id}
               >
                 <Text>
